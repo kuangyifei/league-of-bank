ffcaad1aea3006c02b7397c7f55a10450f5cfc83
kuangyifei <kuangyifei@163.com>
2017-01-19 01:05:29 +0800

    add comments and build full jar spring plugin

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+// This annotation tells Spring Boot to “guess” how you will want to configure Spring,
+// based on the jar dependencies that you have added.
 @EnableAutoConfiguration
 public class SampleController {
 
@@ -20,6 +22,8 @@ public class SampleController {
     }
 
     public static void main(String[] args) throws Exception {
+        // We need to pass Example.class as an argument to the run method to tell
+        // SpringApplication which is the primary Spring component.
         SpringApplication.run(SampleController.class, args);
     }
 }
